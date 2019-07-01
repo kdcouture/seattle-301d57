853601a04775ea4b8249c60a7ea4c0782adc5844
Kevin Couture <kcouture939@gmail.com>
2019-07-01 11:15:47 -0700

    Small change

--- class-00/lab-b/starter-code/app.js
+++ class-00/lab-b/starter-code/app.js
@@ -198,7 +198,7 @@ Student.prototype.scopeArrow = () => console.log(this);
 // DONE: Uncomment the following line of code to see the output in the browser console
 console.log(joe.scopeArrow());
 
-// TODO: Write a COMMENT below to answer the following questions.
+// Done: Write a COMMENT below to answer the following questions.
 // 1. What is "this" when joe.scope() is invoked?
 //  The global window.
 // 2. What is "this" when joe.scopeArrow() is invoked?
